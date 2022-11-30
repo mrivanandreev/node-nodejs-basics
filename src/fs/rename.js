12eaaf445511221ecf78da7a37f306fa315c1d96
Ivan Andreev <mr.ivan.andreev@gmail.com>
2022-11-30 21:49:10 +0300

    feat: add rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { existsSync } from 'node:fs';
+import { rename as rn } from 'node:fs/promises';
+import { ERROR_MESSAGE } from '../utils/constants.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const rename = async () => {
-    // Write your code here 
+  const oldName = resolve(__dirname, 'files/wrongFilename.txt');
+  const newName = resolve(__dirname, 'files/properFilename.md');
+
+  const oldFileExist = existsSync(oldName);
+  const newFileExist = existsSync(newName);
+
+  if (!oldFileExist || newFileExist) {
+    throw new Error(ERROR_MESSAGE);
+  }
+
+  try {
+    await rn(oldName, newName);
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
-await rename();
+rename();
