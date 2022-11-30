dcf5c1937c0b4afb2f624061019c1b4e0df9bbde
Ivan Andreev <mr.ivan.andreev@gmail.com>
2022-11-30 19:33:34 +0300

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { cp } from 'node:fs/promises';
+import { ERROR_MESSAGE } from '../utils/constants.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await cp(resolve(__dirname, 'files'), resolve(__dirname, 'files_copy'), {
+      recursive: true,
+      force: false,
+      errorOnExist: true,
+    });
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await copy();
