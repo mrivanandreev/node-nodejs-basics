60f0631826cd2539386e9ae38d881dc1e6151ec3
Ivan Andreev <mr.ivan.andreev@gmail.com>
2022-12-04 03:23:09 +0300

    feat: add parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+  const resultArray = [];
+
+  for (let i = 0; i < argv.length; i++) {
+    if (argv[i].startsWith('--')) {
+      resultArray.push(`${argv[i].slice(2)} is ${argv[i + 1]}`);
+      i++;
+    }
+  }
+
+  console.log(resultArray.join(', '));
 };
 
 parseArgs();
