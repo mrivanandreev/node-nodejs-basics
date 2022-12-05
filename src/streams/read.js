7d242d40b0c847b3e8f8ff5dc8498e255df66b9f
Ivan Andreev <mr.ivan.andreev@gmail.com>
2022-12-05 22:48:10 +0300

    feat: add streams subtask

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import { createReadStream } from 'node:fs';
+import { join } from 'node:path';
+import { getDirname } from '../utils/getDirname.js';
+
+const __dirname = getDirname(import.meta.url);
+
 const read = async () => {
-    // Write your code here 
+    const stream = createReadStream(join(__dirname, 'files/fileToRead.txt'), { encoding: 'utf-8' });
+    stream.pipe(process.stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import { createWriteStream } from 'node:fs'
+import { join } from 'node:path';
+import { getDirname } from '../utils/getDirname.js';
+
+const __dirname = getDirname(import.meta.url);
+
 const write = async () => {
-    // Write your code here 
+    const stream = createWriteStream(join(__dirname, 'files/fileToWrite.txt'), { encoding: 'utf-8' });
+    process.stdin.pipe(stream);
 };
 
 await write();
