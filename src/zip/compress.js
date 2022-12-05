1dff5e33ae34e7ba9cc50d4dd4bf40f34202b438
Ivan Andreev <mr.ivan.andreev@gmail.com>
2022-12-05 23:34:41 +0300

    feat: add zlib subtask solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { pipeline } from 'node:stream';
+import { join } from 'node:path';
+import { createGzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { getDirname } from '../utils/getDirname.js';
+
+const __dirname = getDirname(import.meta.url);
+
 const compress = async () => {
-    // Write your code here 
+  const input = createReadStream(join(__dirname, 'files/fileToCompress.txt'));
+  const gzip = createGzip();
+  const output = createWriteStream(join(__dirname, 'files/archive.gz'));
+
+  pipeline(input, gzip, output, (error) => {
+    if (error) {
+      console.error('Pipeline failed.', err);
+    }
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import { pipeline } from 'node:stream';
+import { join } from 'node:path';
+import { createUnzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { getDirname } from '../utils/getDirname.js';
+
+const __dirname = getDirname(import.meta.url);
+
 const decompress = async () => {
-    // Write your code here 
+  const input = createReadStream(join(__dirname, 'files/archive.gz'));
+  const gzip = createUnzip();
+  const output = createWriteStream(join(__dirname, 'files/fileToCompress.txt'));
+
+  pipeline(input, gzip, output, (error) => {
+    if (error) {
+      console.error('Pipeline failed.', err);
+    }
+  });
 };
 
 await decompress();
