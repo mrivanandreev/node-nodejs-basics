297956bce40ff81dc8f60738506d00a07c84b1aa
Ivan Andreev <mr.ivan.andreev@gmail.com>
2022-12-05 03:14:02 +0300

    feat: add hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { getDirname } from '../utils/getDirname.js';
+
+const { createHash } = await import('node:crypto');
+
+const __dirname = getDirname(import.meta.url);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const content = await readFile(
+    join(__dirname, 'files/fileToCalculateHashFor.txt'),
+    {
+      encoding: 'utf-8',
+    },
+  );
+
+  console.log(createHash('sha256').update(content).digest('hex'));
 };
 
-await calculateHash();
+calculateHash();
